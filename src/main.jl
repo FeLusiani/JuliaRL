9f669fe3f962e5d94eaf29cefa928d9c786169a4
FeLusiani <>
2020-10-24 13:53:20 +0000

    Added OpenAIGym

--- src/main.jl
+++ src/main.jl
@@ -1,1 +1,15 @@
-include("../dev/Reinforce/examples/mountain_car.jl")
+# using Pkg
+
+# Pkg.add(PackageSpec(url="https://github.com/JuliaML/OpenAIGym.jl.git"))
+
+using OpenAIGym
+env = GymEnv(:BipedalWalker, :v2)
+for i ∈ 1:20
+  T = 0
+  R = run_episode(env, RandomPolicy()) do (s, a, r, s′)
+    render(env)
+    T += 1
+  end
+  @info("Episode $i finished after $T steps. Total reward: $R")
+end
+close(env)
